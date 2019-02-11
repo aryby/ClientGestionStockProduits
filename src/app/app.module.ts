4fdcff38f7c8d04a6ef0f088fe348156a7767573
AZZEDINE EL ARYBY <aryby00@gmail.com>
2019-02-11 22:40:18 +0100

    ajouter le module de routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { Produit } from './shared/produit';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContentComponent } from './content/content.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from './app.routing.module';
 
 
 @NgModule({
@@ -17,10 +19,12 @@ import { ContentComponent } from './content/content.component';
     ProduitComponent,
     NavbarComponent,
     SidebarComponent,
-    ContentComponent
+    ContentComponent,
+    DashboardComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule
   ],
   providers: [ProduitMockService],
   bootstrap: [AppComponent]
